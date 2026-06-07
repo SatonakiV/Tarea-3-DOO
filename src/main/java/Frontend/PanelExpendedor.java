7842ea9c21f2394c0216001163f9eeb5a93a1c43
SatonakiV <difuentes2025@udec.cl>
2026-06-07 18:01:41 -0400

    Agregacion de objeto visual: icono de dinero + saldo predeterminado del usuario

--- src/main/java/Frontend/PanelExpendedor.java
+++ src/main/java/Frontend/PanelExpendedor.java
@@ -1,15 +1,35 @@
 package Frontend;
 
 import Backend.Expendedor;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
 public class PanelExpendedor {
     private Expendedor expLogico;
     private Image imagenMaquina;
+    private Image imagenDinero;
 
     public PanelExpendedor(Expendedor exp) {
         this.expLogico = exp;
+
+        try{
+            java.net.URL urlDinero = getClass().getResource("/dinero.png");
+
+            if(urlDinero != null) {
+                imagenDinero = ImageIO.read(urlDinero);
+            }
+
+            else {
+                System.err.println("Error al obtener imagen dinero...");
+            }
+        }
+
+        catch(Exception e) {
+            System.err.println("Error al obtener imagen dinero..." + e.getMessage());
+        }
+
 
         try {
             imagenMaquina = new ImageIcon(getClass().getResource("/Exp_Frontal.png")).getImage();
@@ -25,5 +45,19 @@ public class PanelExpendedor {
             g.setColor(new Color(90, 92, 92));
             g.fillRect(50, 50, 400, 550);
         }
+
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        if(imagenDinero != null){
+            g.drawImage(imagenDinero, 770, 35, 30, 30, null);
+        }
+
+        g.setFont(new Font("Arial", Font.BOLD, 18));
+        g.setColor(new Color(30, 28, 28));
+
+        g.drawString("Saldo: $4000", 810, 56);
+
     }
 }
