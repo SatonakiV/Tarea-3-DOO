7f3dc9af5725b91169aa7904856c34d5e8010bcf
Gerhec <gerhecp@gmail.com>
2026-06-07 09:48:32 -0400

    dibujo de interfaz de de la maquina

---
+++ src/main/java/Frontend/PanelExpendedor.java
@@ -0,0 +1,29 @@
+package Frontend;
+
+import Backend.Expendedor;
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelExpendedor {
+    private Expendedor expLogico;
+    private Image imagenMaquina;
+
+    public PanelExpendedor(Expendedor exp) {
+        this.expLogico = exp;
+
+        try {
+            imagenMaquina = new ImageIcon(getClass().getResource("/Exp_Frontal.png")).getImage();
+        } catch (Exception e) {
+            System.out.println("Error al cargar la imagen Exp_Frontal.png");
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        if (imagenMaquina != null) {
+            g.drawImage(imagenMaquina, 50, 20, 420, 620, null);
+        } else {
+            g.setColor(new Color(90, 92, 92))
+            g.fillRect(50, 50, 400, 550);
+        }
+    }
+}
